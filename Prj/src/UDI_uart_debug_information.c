f2f7f2da819a4ac343d86f85a464cb47b662961c
Asif <asif.gusejnov.1997@mail.ru>
2018-09-28 12:06:39 +0300

    конфигурация UART

--- Prj/src/UDI_uart_debug_information.c
+++ Prj/src/UDI_uart_debug_information.c
@@ -47,19 +47,41 @@ void
 UDI_Init_UART3_RxTx(
 unsigned int baudrate)
 {
-	
+    U3MODEBITS.UARTEN = 1;
+    U3STABITS.URXISEL = 0x;
+    U3STABITS.UTXEN = 1; 
+    U3BRG = (64000000u / (16 * baudrate)) - 1u;
+    
 }
 
 void
 UDI_Init_IO_Ports(void)
 {
-	
+    /* UART Tx port AN28/PWM3L/PMD4/RP84/RE4 */
+    /* 101 0100 I/O RP84 */
+    /* U3TX 011011 RPn tied to UART3 Transmit */
+    ANSELEbits.ANSE4 = 0;
+    TRISEbits.TRISE4 = 0;
+    RPOR5bits.RP84R = 0b1010100;
+    
+    /* UART Rx Port RB3/AN3/C2IN1+/VPIO/RPI35 */
+    /* UART3 Receive U3RX RPINR27 U3RXR<6:0> */
+    ANSELBbits.ANSB3 = 0;
+    TRISBbits.TRISB3 = 1;
+    RPINR27BITS.U3RXR = 0b010 011 ;
 }
 
 void
 UDI_Init_DMA_For_Tx(void)
 {
-	
+	DMA3CONBITS.CHEN = 1;
+    DMA3CONBITS.SIZE = 1;
+    DMA3CONBITS.DIR = 1;
+    DMA3CONBITS.AMODE = 0b00;
+    DMA3CONBITS.MODE = 0b00;
+    
+    DMA3REQBITS.IRQSEL = 01010011;
+    
 }
 
 void
